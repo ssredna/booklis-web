0359e960d10ed3382a98fd640c27aaa1d8e6907b
Anders Solberg <ssredna@gmail.com>
2024-01-08 16:44:03 +0100

    add firestore adapter to authjs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,14 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
-import { GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
+import {
+	FIREBASE_CLIENT_EMAIL,
+	FIREBASE_PRIVATE_KEY,
+	GOOGLE_ID,
+	GOOGLE_SECRET,
+	PROJECT_ID
+} from '$env/static/private';
+import { FirestoreAdapter } from '@auth/firebase-adapter';
+import { cert } from 'firebase-admin/app';
 
 export const handle = SvelteKitAuth({
 	providers: [
@@ -8,5 +16,12 @@ export const handle = SvelteKitAuth({
 			clientId: GOOGLE_ID,
 			clientSecret: GOOGLE_SECRET
 		})
-	]
+	],
+	adapter: FirestoreAdapter({
+		credential: cert({
+			projectId: PROJECT_ID,
+			clientEmail: FIREBASE_CLIENT_EMAIL,
+			privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+		})
+	})
 });
