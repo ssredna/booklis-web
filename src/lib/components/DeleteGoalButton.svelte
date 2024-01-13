f2e888ad87adbf57658982d935513869ed629563
Anders Solberg <ssredna@gmail.com>
2024-01-13 09:40:13 +0100

    extract DeleteGoalButton to own component

---
+++ src/lib/components/DeleteGoalButton.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { Loader2, Trash } from 'lucide-svelte';
+	import * as AlertDialog from './ui/alert-dialog';
+	import { Button } from './ui/button';
+	import type { SuperValidated } from 'sveltekit-superforms';
+	import type { DeleteGoalSchema } from '$lib/schemas/deleteGoalSchema';
+	import { superForm } from 'sveltekit-superforms/client';
+
+	export let deleteGoalForm: SuperValidated<DeleteGoalSchema>;
+	export let goalId: string;
+
+	const { delayed, submitting, enhance } = superForm(deleteGoalForm);
+</script>
+
+<form id="deleteGoalForm" method="post" action="?/deleteGoal" use:enhance>
+	<input type="hidden" name="goalId" value={goalId} />
+</form>
+
+<AlertDialog.Root>
+	<AlertDialog.Trigger asChild let:builder>
+		<Button variant="destructive" builders={[builder]}>
+			<Trash class="mr-2 h-4 w-4" />
+			Slett mål
+		</Button>
+	</AlertDialog.Trigger>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>Er du sikker på at du vil slette målet?</AlertDialog.Title>
+			<AlertDialog.Description>
+				Det går ikke ann å angre denne handlingen. All progresjon på dette målet vil bli slettet.
+				Bøkene du har lagt til vil fortsatt være tilgjengelig.
+			</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Avbryt</AlertDialog.Cancel>
+			<AlertDialog.Action asChild let:builder>
+				<Button
+					{...builder}
+					type="submit"
+					form="deleteGoalForm"
+					variant="destructive"
+					disabled={$submitting}
+				>
+					{#if $delayed}
+						<Loader2 class="mr-2 h-4 w-4 animate-spin" />
+					{/if}
+					Slett mål
+				</Button>
+			</AlertDialog.Action>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
--- src/lib/components/Goal.svelte
+++ src/lib/components/Goal.svelte
@@ -9,7 +9,7 @@
 	import ReadBook from './ReadBook.svelte';
 	import * as Card from './ui/card';
 	import { Button } from './ui/button';
-	import { Edit, Loader2, Trash } from 'lucide-svelte';
+	import { Edit, Loader2 } from 'lucide-svelte';
 	import { Label } from './ui/label';
 	import { Input } from './ui/input';
 	import type { SuperValidated } from 'sveltekit-superforms';
@@ -17,12 +17,12 @@
 	import { superForm } from 'sveltekit-superforms/client';
 	import { isOwner } from '$lib/isOwnerStore';
 	import type { DeleteGoalSchema } from '$lib/schemas/deleteGoalSchema';
-	import * as AlertDialog from './ui/alert-dialog';
 	import type { AddBookSchema } from '$lib/schemas/addBookSchema';
 	import ChosenBooksCard from './ChosenBooksCard.svelte';
 	import ActiveBooksCard from './ActiveBooksCard.svelte';
 	import { Separator } from './ui/separator';
 	import ReadBooksCard from './ReadBooksCard.svelte';
+	import DeleteGoalButton from './DeleteGoalButton.svelte';
 
 	export let goal: ReadingGoal;
 	export let createGoalForm: SuperValidated<CreateGoalSchema>;
@@ -39,12 +39,6 @@
 			if (form.valid) isEditing = false;
 		}
 	});
-
-	const {
-		delayed: deleteDelayed,
-		submitting: deleteSubmitting,
-		enhance: deleteEnhance
-	} = superForm(deleteGoalForm);
 
 	$: dateString = dateFormat(goal.deadline, 'yyyy-mm-dd');
 
@@ -160,46 +154,7 @@
 				</Button>
 				<Button variant="destructive" on:click={() => (isEditing = false)}>Avbryt</Button>
 			</div>
-			<AlertDialog.Root>
-				<AlertDialog.Trigger asChild let:builder>
-					<Button variant="destructive" builders={[builder]}>
-						<Trash class="mr-2 h-4 w-4" />
-						Slett mål
-					</Button>
-				</AlertDialog.Trigger>
-				<AlertDialog.Content>
-					<AlertDialog.Header>
-						<AlertDialog.Title>Er du sikker på at du vil slette målet?</AlertDialog.Title>
-						<AlertDialog.Description>
-							Det går ikke ann å angre denne handlingen. All progresjon på dette målet vil bli
-							slettet. Bøkene du har lagt til vil fortsatt være tilgjengelig.
-						</AlertDialog.Description>
-					</AlertDialog.Header>
-					<form id="deleteGoalForm" method="post" action="?/deleteGoal" use:deleteEnhance>
-						<input type="hidden" name="goalId" value={goal.id} />
-					</form>
-					<AlertDialog.Footer>
-						<AlertDialog.Cancel>Avbryt</AlertDialog.Cancel>
-						<AlertDialog.Action asChild let:builder>
-							<Button
-								{...builder}
-								type="submit"
-								form="deleteGoalForm"
-								variant="destructive"
-								disabled={$deleteSubmitting}
-							>
-								{#if $deleteDelayed}
-									<Loader2 class="mr-2 h-4 w-4 animate-spin" />
-								{/if}
-								Slett mål
-							</Button>
-						</AlertDialog.Action>
-					</AlertDialog.Footer>
-				</AlertDialog.Content>
-			</AlertDialog.Root>
-			{#if $page.form?.idError}
-				<p>Klarte ikke å slette, prøv igjen</p>
-			{/if}
+			<DeleteGoalButton goalId={goal.id} {deleteGoalForm} />
 		</Card.Footer>
 	</Card.Root>
 {/if}
