60f5b0a272b08aae627ec4220f23143ee59abc06
Anders Solberg <ssredna@gmail.com>
2024-07-27 14:44:24 +0200

    make book use reactive query instead of one-time query

--- src/lib/components/Book.svelte
+++ src/lib/components/Book.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { client } from '$lib/client';
+	import { useQuery } from '@triplit/svelte';
 	import EditBookModal from './EditBookModal.svelte';
 	import { Button } from './ui/button';
 	import { Edit } from 'lucide-svelte';
@@ -10,29 +11,30 @@
 
 	let { bookId }: BookProps = $props();
 
-	let bookPromise = client.fetchById('books', bookId);
+	let bookData = useQuery(client, client.query('books').id(bookId));
+	let bookArray = $derived(bookData.results ? Array.from(bookData.results) : []);
+	let [_id, book] = $derived(bookArray[0]);
+
 	let showEditBookModal = $state(false);
 </script>
 
-{#await bookPromise then book}
-	{#if book}
-		<article class="flex justify-between">
-			<div>
-				<h4 class="text-lg/none font-bold tracking-tight">{book.title}</h4>
-				<small>
-					{book.totalPages} sider
-				</small>
-			</div>
-			<Button
-				size="icon"
-				variant="outline"
-				class="flex-shrink-0"
-				onclick={() => (showEditBookModal = true)}
-			>
-				<Edit class="size-4" />
-			</Button>
-		</article>
-	{/if}
-{/await}
+{#if bookData.results}
+	<article class="flex justify-between">
+		<div>
+			<h4 class="text-lg/none font-bold tracking-tight">{book.title}</h4>
+			<small>
+				{book.totalPages} sider
+			</small>
+		</div>
+		<Button
+			size="icon"
+			variant="outline"
+			class="flex-shrink-0"
+			onclick={() => (showEditBookModal = true)}
+		>
+			<Edit class="size-4" />
+		</Button>
+	</article>
+{/if}
 
 <EditBookModal {bookId} bind:isOpen={showEditBookModal} />
