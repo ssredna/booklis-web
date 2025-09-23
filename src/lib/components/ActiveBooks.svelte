9b1e55f2ce708e6238e41dc0e0b44bd38806df7a
Anders Lensol <ssredna@gmail.com>
2025-09-23 14:23:42 +0200

    remove description from ActiveBooks card

--- src/lib/components/ActiveBooks.svelte
+++ src/lib/components/ActiveBooks.svelte
@@ -11,7 +11,6 @@
 	<Card.Root class="mb-8 w-full max-w-xl">
 		<Card.Header>
 			<Card.Title class="text-2xl font-bold">Aktive bøker</Card.Title>
-			<Card.Description>De bøkene du leser for tiden</Card.Description>
 		</Card.Header>
 
 		<Card.Content class="grid gap-6">
