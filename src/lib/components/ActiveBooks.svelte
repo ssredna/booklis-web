bd1a5802b0cb242b1660311b8bb71d05ef3ee08d
Anders Lensol <ssredna@gmail.com>
2025-09-23 14:07:44 +0200

    refactor ActiveBooksCard into ActiveBooks

--- src/lib/components/ActiveBooks.svelte
+++ src/lib/components/ActiveBooks.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
 	import { getLibrary } from '$lib/state/Library.svelte';
 	import ActiveBook from '$lib/components/ActiveBook.svelte';
-	import ActiveBooksCard from '$lib/components/ActiveBooksCard.svelte';
 	import { Separator } from '$lib/components/ui/separator';
+	import * as Card from '$lib/components/ui/card';
 
 	const library = getLibrary();
 </script>
 
 {#if library.activeBookIds.length > 0}
-	<ActiveBooksCard>
-		{#each library.activeBookIds as activeBookId, i (activeBookId)}
-			<ActiveBook {activeBookId} />
-			{#if i < library.activeBookIds.length - 1}
-				<Separator />
-			{/if}
-		{/each}
-	</ActiveBooksCard>
+	<Card.Root class="mb-8 w-full max-w-xl">
+		<Card.Header>
+			<Card.Title class="text-2xl font-bold">Aktive bøker</Card.Title>
+			<Card.Description>De bøkene du leser for tiden</Card.Description>
+		</Card.Header>
+
+		<Card.Content class="grid gap-6">
+			{#each library.activeBookIds as activeBookId, i (activeBookId)}
+				<ActiveBook {activeBookId} />
+				{#if i < library.activeBookIds.length - 1}
+					<Separator />
+				{/if}
+			{/each}
+		</Card.Content>
+	</Card.Root>
 {/if}
